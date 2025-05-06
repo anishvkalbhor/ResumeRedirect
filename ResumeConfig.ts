a11e4451d87a67f38d8d35d011b3eb27c9a90e17
anishkalbhor123 <anishkalbhor2020@gmail.com>
2025-05-07 02:15:26 +0530

    Added my Credentials and details.

--- ResumeConfig.ts
+++ ResumeConfig.ts
@@ -4,28 +4,28 @@ export const ResumeConfig = {
   // You can use services like Google Drive, Dropbox, or your own server to host the file
   // Make sure to set the file permissions to public or anyone with the link can view
   resumeUrl:
-    "https://drive.google.com/file/d/14ZoBwda8dufni_yts630bKkxF2DSi3Dv/view",
+    "https://drive.google.com/file/d/1Tp3qt2lX1HfL07tZDFa-4MXj9hrCw0ax/view?usp=drive_link",
 
   // Personal Information - these will be displayed on the page
-  name: "Vishal Rajesh Mahajan",
-  designation: "Aspiring Software Developer",
+  name: "Anish Vijay Kalbhor",
+  designation: "Full Stack Developer",
 
   // Social Media Links - these will be displayed as icons on the page
   // You can add or remove social media links as needed
   // Blank links Icon will not be displayed on the page
   socialMedia: {
-    github: "https://github.com/VishalRMahajan",
-    linkedin: "https://linkedin.com/in/VishalRMahajan",
-    portfolio: "https://vishalrmahajan.in",
-    twitter: "https://x.com/VishalRMahajan",
-    email: "mailto:vism06@gmail.com",
+    github: "https://github.com/anishvkalbhor",
+    linkedin: "https://linkedin.com/in/anishvkalbhor",
+    portfolio: "https://portfolio-website-gamma-umber.vercel.app/",
+    twitter: "https://x.com/KalbhorAni14950",
+    email: "mailto:anishkalbhor2020@gmail.com",
   },
 
   // Search Engine Optimization (SEO) - these will be used for the page metadata
   // You can customize the title and description for better visibility on search engines
   seo: {
-    title: "Vishal Mahajan | Resume",
-    description: "Software Developer Resume - Vishal Rajesh Mahajan",
+    title: "Anish Kalbhor | Resume",
+    description: "Full Stack Developer Resume - Anish Vijay Kalbhor",
   },
 
   // Email notification settings
